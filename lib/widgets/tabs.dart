b6e53fa0c4f1636fbc05a8d3448d260281add675
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 10:01:46 +0100

    trabalharabalhando em tabs

--- lib/widgets/tabs.dart
+++ lib/widgets/tabs.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('minha tab'),
+    return Scaffold(
+      appBar: AppBar(title: Text("Kitchen")),
+      body: Container(
+        child: Text("hello"),
+      ),
     );
   }
 }
