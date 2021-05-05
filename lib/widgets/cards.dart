128741dd18a94a564a324a07bec3171062bb574c
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 16:41:00 +0100

    terminado card

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -3,93 +3,101 @@ import 'package:flutter/material.dart';
 class CardsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _mediaQuery = MediaQuery.of(context).size;
     return ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 1), // changes position of shadow
-                ),
-              ],
+      scrollDirection: Axis.horizontal,
+      children: [
+        container(context),
+        container(context),
+      ],
+    );
+  }
+
+  Widget container(BuildContext context) {
+    final _mediaQuery = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.only(right:8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.8),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 1), // changes position of shadow
             ),
-            height: double.infinity,
-            width: _mediaQuery.height * 0.35,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Calulu.jpg/220px-Calulu.jpg',
-                  fit: BoxFit.cover,
-                  height: _mediaQuery.height * 0.18,
-                  width: double.infinity,
+          ],
+        ),
+        height: double.infinity,
+        width: _mediaQuery.height * 0.35,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Calulu.jpg/220px-Calulu.jpg',
+              fit: BoxFit.cover,
+              height: _mediaQuery.height * 0.18,
+              width: double.infinity,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 8),
+              child: Text(
+                "Funge de Carne Seca",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 6),
+              child: Text(
+                "Funge, carne seca de vaca e molho de tomate e algumas folhas de gimboa",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 14,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, top: 8),
-                  child: Text(
-                    "Funge de Carne Seca",
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 6),
+              child: Row(
+                children: [
+                  Text(
+                    "Akz 1300,00",
                     style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, top: 6),
-                  child: Text(
-                    "Funge, carne seca de vaca e molho de tomate e algumas folhas de gimboa",
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.amber,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, top: 6),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Akz 1300,00",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.amber,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 80,
-                      ),
-                      InkWell(
-                        onTap: (){
-                          print("clicou");
-                        },
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            'https://i.ytimg.com/vi/1QqEXNwU1Bw/hqdefault.jpg',
-                            scale: 2.0,
-                          ),
-                        ),
-                      ),
-                    ],
+                  SizedBox(
+                    width: 80,
                   ),
-                ),
-              ],
+                  InkWell(
+                    onTap: () {
+                      print("clicou");
+                    },
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://i.ytimg.com/vi/1QqEXNwU1Bw/hqdefault.jpg',
+                        scale: 2.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      
+          ],
+        ),
+      ),
     );
   }
 }
