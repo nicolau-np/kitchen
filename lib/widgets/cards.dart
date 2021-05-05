8a09fc838d6debe78662569e872a8b5d40f5bab8
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 14:48:16 +0100

    pegando imagem para o primeiro card

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -7,8 +7,25 @@ class CardsPage extends StatelessWidget {
     return Container(
       padding: EdgeInsets.only(left: 8),
       height: _mediaQuery.height * 0.2,
+      width: double.infinity,
       color: Colors.black,
-      child: ListView(),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+         Container(
+           height: double.infinity,
+           width: _mediaQuery.height * 0.5,
+           color: Colors.amber,
+           child: Column(
+             children: [
+               Image.network(''),
+             ],
+           ),
+         ),
+        ],
+      ),
     );
   }
+
+
 }
