5314fff618b04541231a8f22f84cbf8e03e2da22
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 13:56:16 +0100

    mudando colors.amber

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Kitchen",
+      title: "KiTchen KiTchan",
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.amber,
