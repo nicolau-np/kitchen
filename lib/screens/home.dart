e850fb515c615fddc60e7d9d9193d6d4c1a5630a
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 11:24:33 +0100

    chamando carousel na screen home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kitchen/widgets/carousel.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
-    );
+    return Column(children: [
+      CarouselPage(),
+    ],);
   }
 }
