41a0dc00e509f39687f43f753abe1bfd8331e13e
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 11:09:43 +0100

    criado o carousel

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -1,9 +1,27 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 class CarouselPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Carousel(
+          dotSize: 4.0,
+          dotSpacing: 15.0,
+          dotColor: Colors.lightGreenAccent,
+          indicatorBgPadding: 15.0,
+          dotBgColor: Colors.black38,
+          dotVerticalPadding: 0.0,
+          dotPosition: DotPosition.bottomRight,
+          images: [
+            NetworkImage(
+              'https://cdn.jornalgrandebahia.com.br/2017/12/Orando-rezando.jpg',
+            ),
+            NetworkImage(
+              'https://www.cathopic.com/images/1080p/1981d56e5267d4c4863d0c6925db08c5.jpg',
+            ),
+            NetworkImage(
+              'https://st4.depositphotos.com/18349226/20255/i/600/depositphotos_202550970-stock-photo-parent-children-praying-bed-family.jpg',
+            ),
+          ],
+        );
   }
 }
