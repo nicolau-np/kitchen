75d14b04af058ae384c314b23466db585730cd2b
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 09:40:59 +0100

    trabalhando em cozinha screen

---
+++ lib/screens/cozinha.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class CozinhaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          child: Text('cozinha'),
+    );
+  }
+}
