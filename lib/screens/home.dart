1f50ba290a22b59dad75301dfa3ea84b2bf24448
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 14:17:50 +0100

    colocando os componentes na lateral

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CarouselPage(),
         Text("Destaques"),
