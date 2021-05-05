511380e0a5e921bfa39de7a227839a538d82b3d8
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 14:09:37 +0100

    adicionando mediaQuery no carousel

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -2,10 +2,12 @@ import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
 class CarouselPage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+   final _mediaQueryData = MediaQuery.of(context).size;
     return SizedBox(
-      height: 170.0,
+      height: _mediaQueryData.height *0.24,
       width: double.infinity,
       child: Carousel(
         dotSize: 4.0,
