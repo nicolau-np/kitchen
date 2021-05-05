5bb678bb9e7ba4d1c8cee1a21e4e9029a3b0dfaa
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-05 10:14:09 +0100

    mudando estado da tabs para statefull

--- lib/widgets/tabs.dart
+++ lib/widgets/tabs.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class TabsPage extends StatelessWidget {
+class TabsPage extends StatefulWidget {
+  @override
+  _TabsPageState createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
