a5d5eb168b97e1ee7d2be08f74f468dcb8be0482
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-06 13:38:26 +0100

    adicionando cozinha

--- lib/screens/cozinha.dart
+++ lib/screens/cozinha.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 class CozinhaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-          child: Text('cozinha'),
+    return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              
+            ],
+          ),
     );
   }
 }
