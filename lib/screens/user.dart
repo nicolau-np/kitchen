eb7e5aa1116fd7833f434317b27dc96f043659a9
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-06 23:35:29 +0100

    trabalhando em user

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -3,9 +3,22 @@ import 'package:flutter/material.dart';
 class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('User'),
-      
+    final _mediaQueryData = MediaQuery.of(context).size;
+    return Container(
+      height: _mediaQueryData.height * 1,
+      width: double.infinity,
+      color: Colors.black,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Center(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png', scale: 5.0),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
