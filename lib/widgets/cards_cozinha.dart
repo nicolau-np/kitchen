6c44c5026ecfd9008a6cf680c4fe25b3c79355d1
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-06 23:08:42 +0100

    criando radius button no card

--- lib/widgets/cards_cozinha.dart
+++ lib/widgets/cards_cozinha.dart
@@ -12,15 +12,24 @@ class CardsCozinha extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.vertical,
         children: [
-
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
               height: 250.0,
               width: double.infinity,
-              color: Colors.white,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
               child: Column(
-                children: [],
+                children: [
+                  
+                ],
               ),
             ),
           ),
