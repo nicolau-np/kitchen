eafe0ceddfcbb3badf349e254fee1bd707cbfc47
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-06 23:12:19 +0100

    terminado o card da cozinha

--- lib/widgets/cards_cozinha.dart
+++ lib/widgets/cards_cozinha.dart
@@ -27,8 +27,67 @@ class CardsCozinha extends StatelessWidget {
                 ),
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  
+                  Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Calulu.jpg/220px-Calulu.jpg',
+                    fit: BoxFit.cover,
+                    height: _mediaQueryData.height * 0.18,
+                    width: double.infinity,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, top: 8),
+                    child: Text(
+                      "Funge de Carne Seca",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, top: 6),
+                    child: Text(
+                      "Funge, carne seca de vaca e molho de tomate e algumas folhas de gimboa",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, top: 6),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Akz 1300,00",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.amber,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 90,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            print("clicou");
+                          },
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              'https://i.ytimg.com/vi/1QqEXNwU1Bw/hqdefault.jpg',
+                              scale: 2.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text("Mama Cuiba")
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
