1256e1e634f5450bdf31fd2ead26c915f9659beb
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-06 13:57:52 +0100

    update

--- lib/screens/cozinha.dart
+++ lib/screens/cozinha.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:kitchen/widgets/cards_cozinha.dart';
+
 class CozinhaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-                Text("Cozinhas"),
-                CardsCozinha(),
-            ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            "Cozinhas",
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          CardsCozinha(),
+        ],
+      ),
     );
   }
 }
