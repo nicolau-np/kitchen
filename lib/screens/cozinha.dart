690c84b80f400f7faee2cba9388bda0551ef5fb4
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-06 13:51:31 +0100

    criando container do card

--- lib/screens/cozinha.dart
+++ lib/screens/cozinha.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kitchen/widgets/cards_cozinha.dart';
 class CozinhaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/widgets/cards_cozinha.dart
+++ lib/widgets/cards_cozinha.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class CardsCozinha extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final _mediaQueryData = MediaQuery.of(context).size;
+
+    return ListView(
+      children: [
+        Container(
+          height: _mediaQueryData.height * 0.4,
+          width: double.infinity,
+          
+        ),
+      ],
     );
   }
 }
